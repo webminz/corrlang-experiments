a942f09cfa2b3f92208f0a27b16f161333a7e5a8
Patrick Stünkel <webminz@googlemail.com>
2026-02-10 19:00:37 +0100

    adding windows script

---
+++ run.ps1
@@ -0,0 +1,6 @@
+.\corrl.exe down
+.\corrl.exe up
+.\corrl.exe apply -p experiment -f .\experiment_0\spec.corr
+.\corrl.exe apply -p experiment -f .\experiment_1\spec.corr
+.\corrl.exe apply -p experiment -f .\experiment_2\spec.corr
+
